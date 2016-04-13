06376070e3e17ed11ca3b5dae37d1e5e47cab2b9
wangjun <wangjun@frogshealth.com>
2016-04-13 20:26:13 +0800

    create

---
+++ src/main/java/org/jzkangta/tlspc/App.java
@@ -0,0 +1,13 @@
+package org.jzkangta.tlspc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
